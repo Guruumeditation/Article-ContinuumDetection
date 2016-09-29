01ef424548b11c0538cef5a5c1125b6c9a7c18ad
olivier matis <olivier matis>
2016-09-29 21:31:16 +0200

    First commit

---
+++ ContinuumDetection/ContinuumHelper.cs
@@ -0,0 +1,10 @@
+﻿using Windows.System.Profile;
+using Windows.UI.ViewManagement;
+
+namespace ContinuumDetection
+{
+    public static class ContinuumHelper
+    {
+        public static bool IsContinuumMode => (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile") && (UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Mouse);
+    }
+}
---
+++ ContinuumDetection/ContinuumStateTrigger.cs
@@ -0,0 +1,47 @@
+﻿using Windows.System.Profile;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+namespace ContinuumDetection
+{
+    public class ContinuumStateTrigger : StateTriggerBase
+    {
+        public static readonly DependencyProperty StateProperty = DependencyProperty.Register(
+            "State", typeof(ContinuumState), typeof(ContinuumStateTrigger), new PropertyMetadata(default(ContinuumState), PropertyChangedCallback));
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var t = dependencyObject as ContinuumStateTrigger;
+
+            t?.SetActive(t.GetContinuumState() == t.State);
+        }
+
+        public ContinuumState State
+        {
+            get { return (ContinuumState)GetValue(StateProperty); }
+            set { SetValue(StateProperty, value); }
+        }
+
+        public ContinuumStateTrigger()
+        {
+            Window.Current.SizeChanged += CurrentOnSizeChanged;
+        }
+
+        private void CurrentOnSizeChanged(object sender, WindowSizeChangedEventArgs windowSizeChangedEventArgs)
+        {
+            SetActive(GetContinuumState() == State);
+        }
+
+        private ContinuumState GetContinuumState()
+        {
+            return (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile") && (UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Mouse) ? ContinuumState.Continuum : ContinuumState.Base;
+        }
+    }
+
+    public enum ContinuumState
+    {
+        Base = 0,
+        Continuum = 1
+    }
+}
